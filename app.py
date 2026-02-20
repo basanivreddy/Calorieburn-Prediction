d39f4fbd82306c179dbb5e448cc35b0be5aae0f4
vivek reddy <vivekreddy1789@gmail.com>
2026-02-20 20:03:24 +0530

    app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
+import os
 
 app = Flask(__name__)
 
@@ -63,5 +64,8 @@ def home():
     return render_template("index.html", prediction=prediction)
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
